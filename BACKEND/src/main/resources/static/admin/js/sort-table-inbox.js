77d5973ba547bfd861e6658e7fd36decccd1d5b8
Mykola Matios <65984494+Mikolka09@users.noreply.github.com>
2023-03-11 21:17:56 +0100

    11.03.2023

---
+++ BACKEND/src/main/resources/static/admin/js/sort-table-inbox.js
@@ -0,0 +1,157 @@
+$('.sort-table-inbox').on('click', function (e) {
+    let senders = Object.values(list);
+    let column = $(this);
+    let img;
+    let arr = [];
+    let name = column.attr("data-name");
+    let tag = column.attr("data-tag");
+    switch (name) {
+        case "Senders":
+            img = $('#send');
+            if (tag === "down") {
+                let data = senders.sort((x,y)=>x[0].invite.userFrom.fullName.localeCompare(y[0].invite.userFrom.fullName));
+                arr.push(data);
+                printAllSenders(arr, data[0][0].invite.userFrom.fullName);
+                column.attr("data-tag", "up");
+                img.attr("src", "/img/icon/up-arrow.png");
+            } else {
+                let data = senders.sort((x,y)=>y[0].invite.userFrom.fullName.localeCompare(x[0].invite.userFrom.fullName));
+                arr.push(data);
+                printAllSenders(arr, data[0][0].invite.userFrom.fullName);
+                column.attr("data-tag", "down");
+                img.attr("src", "/img/icon/down-arrow.png");
+            }
+            break;
+        default:
+            break;
+    }
+})
+
+function printTableComments(data) {
+    let container = document.getElementById('body-table');
+    container.innerHTML = "";
+    if (data != null) {
+        for (let i = 0; i < data.length; i++) {
+            let tr = document.createElement('tr');
+            let td1 = document.createElement('td');
+            let img1 = document.createElement('img');
+            img1.src = "/" + data[i].post.user.avatarUrl;
+            img1.style.borderRadius = "50%";
+            img1.style.paddingBottom = "5px";
+            img1.width = 50;
+            img1.alt = "avatar";
+            td1.appendChild(img1);
+            let td2 = document.createElement('td');
+            td2.innerText = data[i].post.user.username;
+            let td3 = document.createElement('td');
+            td3.innerText = data[i].post.title;
+            let td4 = document.createElement('td');
+            td4.innerText = data[i].body;
+            let td5 = document.createElement('td');
+            td5.innerText = data[i].post.category.status !== "DELETED" ? data[i].post.category.name : "NULL";
+            let td6 = document.createElement('td');
+            td6.innerText = data[i].status;
+            let td7 = document.createElement('td');
+            td7.innerText = correctDate(data[i].createdAt);
+            let td8 = document.createElement('td');
+            td8.innerText = correctDate(data[i].updatedAt);
+            let td9 = document.createElement('td');
+            let a1 = document.createElement('a');
+            a1.href = "#";
+            a1.title = "Edit";
+            a1.style.marginRight = "4px";
+            a1.dataset.id = data[i].id;
+            a1.dataset.username = data[i].post.user.username;
+            a1.dataset.body = data[i].body;
+            a1.dataset.titlePost = data[i].post.title;
+            a1.dataset.category = data[i].post.category.name;
+            a1.dataset.toggle = "modal";
+            a1.dataset.target = "#EditCommentModal";
+            let img2 = document.createElement('img');
+            img2.src = "/img/icon/edit.png";
+            img2.width = 30;
+            a1.appendChild(img2);
+            td9.appendChild(a1);
+            let a2 = document.createElement('a');
+            a2.hidden = data[i].status !== "DELETED";
+            a2.href = "#";
+            a2.title = "Recovery";
+            a2.dataset.id = data[i].id;
+            a2.dataset.username = data[i].post.user.username;
+            a2.dataset.body = data[i].body;
+            a2.dataset.titlePost = data[i].post.title;
+            a2.dataset.category = data[i].post.category.name;
+            a2.dataset.toggle = "modal";
+            a2.dataset.target = "#RecoveryCommentModal";
+            let img3 = document.createElement('img');
+            img3.src = "/img/icon/restore.png";
+            img3.width = 30;
+            a2.appendChild(img3);
+            td9.appendChild(a2);
+            let a3 = document.createElement('a');
+            a3.hidden = data[i].status === "DELETED";
+            a3.href = "#";
+            a3.title = "Delete";
+            a3.dataset.id = data[i].id;
+            a3.dataset.name = data[i].body;
+            a3.dataset.toggle = "modal";
+            a3.dataset.target = "#DeleteModal";
+            let img4 = document.createElement('img');
+            img4.src = "/img/icon/delete.png";
+            img4.width = 30;
+            a3.appendChild(img4);
+            td9.appendChild(a3);
+
+            tr.append(td1, td2, td3, td4, td5, td6, td7, td8, td9);
+            container.append(tr);
+        }
+    }
+}
+
+$('#button-search').on('click', function () {
+    let input = $('#input-search-admin');
+    let text = input.val();
+    if (text !== "") {
+        input.val('');
+        searchComment(text);
+    }
+})
+
+function searchComment(text) {
+    let comments = data;
+    let arrText = text.split(' ');
+    let result = [];
+    let answer = "";
+    for (let txt of arrText) {
+        for (let i = 0; i < comments.length; i++) {
+            if (comments[i].post.user.username.toLowerCase().includes(txt.toLowerCase()) ||
+                comments[i].post.title.toLowerCase().includes(txt.toLowerCase()) ||
+                comments[i].status.toLowerCase().includes(txt.toLowerCase())) {
+                if (result.length === 0)
+                    result.push(comments[i]);
+                else {
+                    if (result.filter(x => x.id === comments[i].id).length === 0)
+                        result.push(comments[i]);
+                }
+            } else {
+                if (comments[i].body.toLowerCase().includes(txt.toLowerCase()) ||
+                    comments[i].post.category.name.toLowerCase().includes(txt.toLowerCase())) {
+                    if (result.length === 0)
+                        result.push(comments[i]);
+                    else {
+                        if (result.filter(x => x.id === comments[i].id).length === 0)
+                            result.push(comments[i]);
+                    }
+                }
+            }
+        }
+    }
+    if (result.length !== 0) {
+        answer = "Search result - \"" + text.toUpperCase() + "\"";
+        alertInfo(answer);
+        printTableComments(result);
+    } else {
+        answer = "Search result - \"" + text.toUpperCase() + "\" returned nothing!";
+        alertInfo(answer);
+    }
+}
